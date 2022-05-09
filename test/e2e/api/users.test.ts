90e34a7fdf2753810aaa72793c76517ff471755a
Binh-Phi Nguyen <kugoo109@gmail.com>
2022-05-09 22:54:54 +0700

    add discover, like, pass, likes, matches testCases

--- test/e2e/api/users.test.ts
+++ test/e2e/api/users.test.ts
@@ -116,4 +116,137 @@ describe("api/users", () => {
 
       .expect(404);
   });
+
+  test("GET: /discover should return list user discover", async () => {
+    // Create user
+    const createdUser1 = await createUser({});
+
+    const request = supertest(app);
+
+    const response = await request.get(`/users/discover?userId=${createdUser1._id}`).expect(200);
+
+    expect(response.body).not.toBeNull();
+    expect(response.body).toHaveLength(7);
+    expect(response.body).toEqual(
+      expect.not.arrayContaining([expect.objectContaining({ _id: createdUser1._id })])
+    );
+  });
+
+  test("POST: /pass should pass user successful", async () => {
+    // Create user
+    const createdUser1 = await createUser({});
+    const createdUser2 = await createUser({});
+
+    const request = supertest(app);
+    const response = await request
+      .post(`/users/pass`)
+      .send({
+        userFromId: createdUser1._id,
+        userToId: createdUser2._id,
+      })
+      .expect(200);
+
+    expect(response.body).not.toBeNull();
+    expect(response.body.success).toEqual(true);
+  });
+
+  test("POST: /pass should return 404 if user not existed", async () => {
+    const request = supertest(app);
+    await request
+      .post(`/users/pass`)
+      .send({
+        userFromId: Types.ObjectId(),
+        userToId: Types.ObjectId(),
+      })
+      .expect(404);
+  });
+
+  test("POST: /like should like user successful", async () => {
+    // Create user
+    const createdUser1 = await createUser({});
+    const createdUser2 = await createUser({});
+
+    const request = supertest(app);
+    const response = await request
+      .post(`/users/like`)
+      .send({
+        userFromId: createdUser1._id,
+        userToId: createdUser2._id,
+      })
+      .expect(200);
+
+    expect(response.body).not.toBeNull();
+    expect(response.body.success).toEqual(true);
+    expect(response.body.matched).toEqual(false);
+  });
+
+  test("POST: /like should return 404 if user not existed", async () => {
+    const request = supertest(app);
+    await request
+      .post(`/users/like`)
+      .send({
+        userFromId: Types.ObjectId(),
+        userToId: Types.ObjectId(),
+      })
+      .expect(404);
+  });
+
+  test("GET: /:userId/likes should return list user likes", async () => {
+    // Create user
+    const createdUser1 = await createUser({});
+    const createdUser2 = await createUser({});
+    const createdUser3 = await createUser({});
+
+    const request = supertest(app);
+
+    await request
+      .post(`/users/like`)
+      .send({
+        userFromId: createdUser1._id,
+        userToId: createdUser2._id,
+      })
+      .expect(200);
+
+    await request
+      .post(`/users/like`)
+      .send({
+        userFromId: createdUser1._id,
+        userToId: createdUser3._id,
+      })
+      .expect(200);
+
+    const response = await request.get(`/users/${createdUser1._id}/likes`).expect(200);
+
+    expect(response.body).not.toBeNull();
+    expect(response.body).toHaveLength(2);
+  });
+
+  test("GET: /:userId/matches should return list user matches", async () => {
+    // Create user
+    const createdUser1 = await createUser({});
+    const createdUser2 = await createUser({});
+
+    const request = supertest(app);
+
+    await request
+      .post(`/users/like`)
+      .send({
+        userFromId: createdUser1._id,
+        userToId: createdUser2._id,
+      })
+      .expect(200);
+
+    await request
+      .post(`/users/like`)
+      .send({
+        userFromId: createdUser2._id,
+        userToId: createdUser1._id,
+      })
+      .expect(200);
+
+    const response = await request.get(`/users/${createdUser1._id}/matches`).expect(200);
+
+    expect(response.body).not.toBeNull();
+    expect(response.body).toHaveLength(1);
+  });
 });
